b5911e28e26f1f1f8daa3da8fc79b23be467c8fe
JKK86 <j.kurowski86@wp.pl>
2021-06-27 15:34:08 +0200

    add reservation model

--- courts/models.py
+++ courts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from squash_club import settings
+
 
 class Category(models.Model):
     name = models.CharField(max_length=32, verbose_name="Nazwa")
@@ -27,3 +29,22 @@ class Court(models.Model):
         verbose_name_plural = "Korty"
         ordering = ("number", )
 
+
+class Reservation(models.Model):
+    court = models.ForeignKey(Court, on_delete=models.CASCADE, related_name="reservations", verbose_name="Kort")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="Użytkownik")
+    date = models.DateField(verbose_name="Data rezerwacji")
+    time = models.TimeField(verbose_name="Godzina rezerwacji")
+    duration = models.PositiveIntegerField(verbose_name="Czas rezerwacji", default=1)
+    paid = models.BooleanField(default=False, verbose_name="Opłacone")
+    status = models.BooleanField(default=False, verbose_name="Status")
+    comments = models.TextField(verbose_name="Komentarz", null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Data utworzenia")
+
+    def __str__(self):
+        return f"Rezerwacja nr {self.id}"
+
+    class Meta:
+        verbose_name = "rezerwacja"
+        verbose_name_plural = "Rezerwacje"
+        constraints = [models.UniqueConstraint(fields=["court", "date", "time"], name="unique_reservation")]
