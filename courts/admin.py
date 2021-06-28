6461b73d2046122c716d85db5cc01052405b5090
JKK86 <j.kurowski86@wp.pl>
2021-06-28 11:55:38 +0200

    register reservation model in admin panel

--- courts/admin.py
+++ courts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from courts.models import Category, Court
+from courts.models import Category, Court, Reservation
 
 
 @admin.register(Category)
@@ -13,3 +13,11 @@ class CategoryAdmin(admin.ModelAdmin):
 class CourtAdmin(admin.ModelAdmin):
     list_display = ['category', 'number', 'air_condition', 'lighting', ]
     list_filter = ['category', ]
+
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ['court', 'user', 'date', 'time', 'duration', 'paid', 'status', 'comments', 'created']
+    list_filter = ['court', 'date', 'paid', 'status']
+    search_fields = ['user']
+    exclude = ['paid', 'created']
